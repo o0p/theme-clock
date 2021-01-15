e0fd8024972bace5897c60d786e00de171e3bf73
Russell <russelldev@protonmail.com>
2021-01-15 20:27:50 +0000

    Add days and months arrs  style

--- app.js
+++ app.js
@@ -4,3 +4,6 @@ const secondEl = document.querySelector('.second');
 const timeEl = document.querySelector('.time');
 const dateEl = document.querySelector('.date');
 const toggleEl = document.querySelector('.toggle');
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const days = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
