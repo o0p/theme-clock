b2c5b2dde3540edc1faec47fe17f110efc568dd5
Russell <russelldev@protonmail.com>
2021-01-15 21:06:53 +0000

    Map hands

--- app.js
+++ app.js
@@ -28,5 +28,15 @@ function setTime() {
     const minutes = time.getMinutes();
     const seconds = time.getSeconds();
 
+    hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`;
+    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`;
+    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`;
+
 
 }
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+};
+
+setTime();
