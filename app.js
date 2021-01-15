ec84bb755c46297beadeb1e9560b742c96c915f7
Russell <russelldev@protonmail.com>
2021-01-15 20:49:09 +0000

    Add dark mode funct style

--- app.js
+++ app.js
@@ -3,7 +3,18 @@ const minuteEl = document.querySelector('.minute');
 const secondEl = document.querySelector('.second');
 const timeEl = document.querySelector('.time');
 const dateEl = document.querySelector('.date');
-const toggleEl = document.querySelector('.toggle');
+const toggle = document.querySelector('.toggle');
 
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-const days = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+toggle.addEventListener('click', function(event) {
+    const html = document.querySelector('html');
+    if (html.classList.contains('dark')) {
+        html.classList.remove('dark');
+        event.target.innerHTML = 'Dark mode';
+    } else {
+        html.classList.add('dark');
+        event.target.innerHTML = 'Dark mode';
+    }
+});
